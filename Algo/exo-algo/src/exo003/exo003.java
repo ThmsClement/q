86b2a28b903b92d77231148b48b226403333413d
Wisee59 <quentin.surmont@gmail.com>
2024-08-23 21:05:43 +0200

    update name

--- Algo/exo-algo/src/exo003/exo003.java
+++ Algo/exo-algo/src/exo003/exo003.java
@@ -1,6 +1,6 @@
 package exo003;
 
-public class Prout {
+public class exo003 {
     public static void main(String[] args) {
         System.out.println("Prout");
         System.out.println("Prout2");
