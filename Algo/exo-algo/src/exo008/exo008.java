3f7a47fe94b11031373eb58224af3b41c39c9efd
tclement <thomas.clement@gim.be>
2024-09-12 21:49:37 +0200

    Correction 08 et 09

--- Algo/exo-algo/src/exo008/exo008.java
+++ Algo/exo-algo/src/exo008/exo008.java
@@ -4,22 +4,44 @@ import java.util.Scanner;
 
 public class exo008 {
     public static void main(String[] args) {
-        int balles = 1;
+
+//        System.out.println("Etês vous prêt à recevoir la balle :");
+//        Scanner sc = new Scanner(System.in);
+//        boolean ok = Boolean.parseBoolean(sc.nextLine());
+//
+//        System.out.println("Le panier est il vide? :");
+//        boolean panierVide = Boolean.parseBoolean(sc.nextLine());
+//
+//
+//        if (ok & !panierVide) {
+//            System.out.println("Je lance la balle");
+//        }
+//        else {
+//            System.out.println("Je ne lance pas la balle car");
+//            if (panierVide) {
+//                System.out.println("Le panier est vide");
+//            }
+//            if (!ok) {
+//                System.out.println("Vous n'êtes pas prêt");
+//            }
+//        }
+
+
         System.out.println("Etês vous prêt à recevoir la balle :");
         Scanner sc = new Scanner(System.in);
         boolean ok = Boolean.parseBoolean(sc.nextLine());
-        if (ok == true & balles >= 0) {
-            if ( ok == true & balles == 0){
-                System.out.println("panier vide");
+
+        if (ok) {
+            System.out.println("Le panier est il vide? :");
+            boolean panierVide = Boolean.parseBoolean(sc.nextLine());
+            if (!panierVide) {
+                System.out.println("Je lance la balle");
+            } else {
+                System.out.println("Je ne lance pas la balle car le panier est vide");
             }
-            else {
-                System.out.println("Lancer balle");
-            }
+        } else {
+            System.out.println("Je ne lance pas la balle car");
+            System.out.println("Vous n'êtes pas prêt");
         }
-        else {
-            System.out.println("Lanceur pas prêt");
-        }
-        int balle = balles - 1;
-        balles  = balle;
     }
 }
--- Algo/exo-algo/src/exo009/exoo009.java
+++ Algo/exo-algo/src/exo009/exoo009.java
@@ -4,38 +4,48 @@ import java.util.Scanner;
 
 public class exoo009 {
     public static void main(String[] args) {
-        int coca = 0;
+        int coca = 2;
         int eau = 0;
-        int fanta = 0;
+        int fanta = 1;
+
         System.out.println("Merci de taper le chiffre correspondant à la boisson désirée :");
         System.out.println("1 = coca\n2 = eau\n3 = fanta");
+
         Scanner sc = new Scanner(System.in);
         int choix = Integer.parseInt(sc.nextLine());
+
         switch (choix) {
             case 1:
-                if (coca > 0) {
+                if (coca != 0) {
                     System.out.println("Profitez de votre coca biens frais");
-                }
-                else {
+                    coca = coca - 1;
+                } else {
                     System.out.println("Il n'y a plus de coca dans la machine");
                 }
                 break;
-                case 2:
-                    if (eau > 0) {
-                        System.out.println("L'eau c'est bon pour la santé!");
-                    }
-                    else {
-                        System.out.println("Il n'y a plus d'eau dans la machine");
-                    }
+            case 2:
+                if (eau != 0) {
+                    System.out.println("L'eau c'est bon pour la santé!");
+                    eau -= 1;
+                } else {
+                    System.out.println("Il n'y a plus d'eau dans la machine");
+                }
+                break;
+            case 3:
+                if (fanta != 0) {
+                    System.out.println("Fantaaaaaa!");
+                    fanta--;
+                } else {
+                    System.out.println("Il n'y a plus de fanta dans la machine");
                     break;
-                    case 3:
-                        if (fanta > 0) {
-                            System.out.println("Fantaaaaaa!");
-                        }
-                        else {
-                            System.out.println("Il n'y a plus de fanta dans la machine");
-                            break;
-                        }
+                }
+            default:
+                System.out.println("Le choix n'est pas valide");
+                break;
         }
+
+        System.out.println("Il reste " + coca + " coca");
+        System.out.println("Il reste " + eau + " eau");
+        System.out.println("Il reste " + fanta + " fanta");
     }
 }
