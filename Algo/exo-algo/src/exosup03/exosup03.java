52e7faf57e709eede65425f49abcd390b4b7ed53
Wisee59 <quentin.surmont@gmail.com>
2024-10-20 20:29:12 +0200

    exo JustePrixOpti

---
+++ Algo/exo-algo/src/exosup03/exosup03.java
@@ -0,0 +1,55 @@
+package exosup03;
+
+import java.util.Scanner;
+
+public class exosup03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] tableau = new int[10];
+        int i = 0;
+        int resultat = 0;
+        String chaine = "a";
+
+
+        while (i < 10) {
+            System.out.println("Merci d'entrer un Entier");
+            String entier = sc.nextLine();
+            if (entier.isEmpty()) {
+            } else {
+                try {
+                    tableau[i] = Integer.parseInt(entier);
+                    if (i <10){
+                        i++;
+                    }
+                } catch (NumberFormatException e) {
+                }
+            }
+
+        }
+        i = 0;
+        if (tableau[i] < tableau[i + 1]) {
+            resultat = tableau[i];
+        } else {
+            resultat = tableau[i + 1];
+        }
+        i++;
+        while (i < 9) {
+            if (resultat > tableau[i + 1]) {
+                resultat = tableau[i + 1];
+            }
+            i++;
+        }
+        System.out.println("Voici le plus petit entier entré par vos soins : " + resultat);
+    }
+
+
+    public static boolean estEntier(String chaine) {
+        try {
+            Integer.parseInt(chaine);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+}
+
---
+++ Algo/exo-algo/src/exosup04/exosup04.java
@@ -0,0 +1,110 @@
+package exosup04;
+
+import java.util.Scanner;
+
+public class exosup04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String tiret = "-";
+        String star = "*";
+        int nbTiret = 1;
+        int nbStar = 1;
+        int nbRep = 0;
+        int nbFin = 0;
+        boolean choixTiret = true;
+        boolean choixStar = true;
+        boolean choixRep = true;
+
+        System.out.println("Veuiller entrer un nombre de Tirets");
+
+        while (choixTiret) {
+            String choix = sc.nextLine();
+            if (choix.isEmpty()) {
+                System.out.println("Veuiller entrer un nombre de Tirets");
+            } else {
+                try {
+                    nbTiret = Integer.parseInt(choix);
+                    choixTiret = false;
+                } catch (NumberFormatException e) {
+                    System.out.println("Veuiller entrer un nombre de Tirets");
+
+                }
+            }
+        }
+
+        System.out.println("Veuiller entrer un nombre d'étoiles");
+        while (choixStar) {
+            String choix = sc.nextLine();
+            if (choix.isEmpty()) {
+                System.out.println("Veuiller entrer un nombre de Tirets");
+            } else {
+                try {
+                    nbStar = Integer.parseInt(choix);
+                    choixStar = false;
+                } catch (NumberFormatException e) {
+                    System.out.println("Veuiller entrer un nombre de Tirets");
+
+                }
+            }
+        }
+
+        System.out.println("Veuiller entrer un nombre de répétitions");
+        while (choixRep) {
+            String choix = sc.nextLine();
+            if (choix.isEmpty()) {
+                System.out.println("Veuiller entrer un nombre de Tirets");
+            } else {
+                try {
+                    nbRep = Integer.parseInt(choix);
+                    choixRep = false;
+                } catch (NumberFormatException e) {
+                    System.out.println("Veuiller entrer un nombre de Tirets");
+
+                }
+            }
+        }
+        choixTiret = true;
+        choixStar = true;
+        boolean fin = true;
+        int incrementation = 1;
+
+        while (fin) {
+            while (choixTiret) {
+                System.out.print(tiret);
+                if (nbTiret == incrementation) {
+                    choixTiret = false;
+                }
+                incrementation++;
+            }
+            choixTiret = true;
+            incrementation = 1;
+
+            while (choixStar) {
+                System.out.print(star);
+
+                if (nbStar == incrementation) {
+                    choixStar = false;
+                }
+                incrementation++;
+            }
+            choixStar = true;
+            incrementation = 1;
+
+            nbFin++;
+            if (nbFin == nbRep){
+                fin = false;
+            }
+
+        }
+
+    }
+
+    public static boolean estEntier(String chaine) {
+        try {
+            Integer.parseInt(chaine);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+}
